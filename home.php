6cc25921ab4f6b267ff68c8702002f58f3577037
gstrenge <gstrenge01@gmail.com>
2017-08-08 10:32:55 -0400

    Added Home page when not logged in, asked the user to go to login page

--- home.php
+++ home.php
@@ -1,13 +1,51 @@
 <?php
 session_start();
-
-
-if($_SESSION['login'])
-{
-	$user = $_SESSION['user'];
-	echo "Hello $user";
-}
-
-
-
 ?>
+
+<html>
+
+<?php
+if(isset($_SESSION['login']))
+{
+	if ($_SESSION['login'] && isset($_SESSION['user']))
+	{
+		$user = $_SESSION['user'];
+	
+?>
+
+	<head>
+		<title>Login</title>
+		<center><?php  echo "Hello $user"; ?></center>
+	</head>
+	
+	<body>
+	
+		<p>
+			Welcome
+		</p>
+
+	</body>
+<?php
+	}
+}
+if(!isset($_SESSION['login']))
+{
+	echo "You are not logged in!";
+	?>
+	
+	<head>
+		<title>(Not Logged in)</title>
+	</head>
+	
+	<body>
+		<p>
+			Incorrect Login Information
+			<form action = 'root.php'>
+				<input type = 'submit' value = 'Retry'>
+			</form>
+		</p>
+	</body>
+<?php
+}
+?>
+</html>
